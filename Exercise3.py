b9f2fc6c352bdd4e0c9bfb87646d84ab8d46b638
Dmitriy <dimgod80@yandex.ru>
2022-12-18 00:06:43 +0800

    update Exercise3

--- Exercise3.py
+++ Exercise3.py
@@ -5,14 +5,14 @@
 # - Для n = 6: {1: 2, 2: 2,25, 3: 3,37, 4: 2,44, 5: 2,49, 6: 2,52}
 
 num = int(input("Введите число: "))
-my_list = {}
+my_dict = {}
 
 for i in range(1, num + 1):
-    my_list[i] = round(((1 + (1 / i)) ** i), 2)
+    my_dict[i] = round(((1 + (1 / i)) ** i), 2)
 
 summ = 0
-for i in range(1, len(my_list) + 1):
-    summ += my_list[i]
+for i in range(1, len(my_dict) + 1):
+    summ += my_dict[i]
 
-print(my_list)
+print(my_dict)
 print(f"Сумма чисел в словаре равна {summ}")
